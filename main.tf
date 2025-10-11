7ce1dd522fde6054f96d03b16cbdd0b8ea1f3337
Rajkumar B <rajkumaryadavb54@gmail.com>
2025-10-11 13:28:39 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -6,4 +6,7 @@ resource "aws_instance" {
   ami = "ami-0360c520857e3138f" 
   instance_type = "t2.micro"
   }
-  
+  output "ec2_public_ip" {
+   description = "public ip address of an EC2 instance"
+value = "aws_instance.ec2.public_ip
+}
