fd0e84f3bc29635f089b0531e14c67129dde916d
Rajkumar B <rajkumaryadavb54@gmail.com>
2025-10-19 15:44:01 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,12 +1,21 @@
 provider "aws" {
-    region = "us-east-1" 
+    region = "eu-north-1" 
     }
 
 resource "aws_instance" {
   ami = "ami-0360c520857e3138f" 
-  instance_type = "t2.micro"
+  instance_type = "t3.micro"
   }
   output "ec2_public_ip" {
    description = "public ip address of an EC2 instance"
-value = "aws_instance.ec2.public_ip
+value = "aws_instance.ec2.public_ip"
+}
+
+resource "aws_instance" {
+  ami = ""
+  instnace_type = "t3.micro"
+}
+output "ec2_public_ip" {
+  description = "public ip address of an 2nd EC2 instance"
+value = "aws_2ndinstance.ec2.public_ip"
 }
